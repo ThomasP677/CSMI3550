70be72b4663639335d2da14117bcd8564db71d2e
ThomasP677 <85772082+ThomasP677@users.noreply.github.com>
2024-05-03 20:07:57 -0700

    Update Homework1.rs

--- Homework1.rs
+++ Homework1.rs
@@ -1,1 +1,21 @@
 
+extern crate if_addrs;
+
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+
+fn main() {
+    let interfaces = if_addrs::get_if_addrs().expect("Failed to get network interfaces");
+    for interface in interfaces {
+        if let Some(ip) = interface.ip() {
+            if !ip.is_loopback() && !ip.is_link_local() {
+                match ip {
+                    IpAddr::V4(ipv4) => {
+                        println!("Local IPv4 Address: {}", ipv4);
+                    }
+                    IpAddr::V6(ipv6) => {
+                        println!("Local IPv6 Address: {}", ipv6);
+                    }
+                }
+            }
+        }
+    }
